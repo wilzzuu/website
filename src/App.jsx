c026c5a533f49a7f5319a44916af9258dba472c5
lexyxc <elias.w.terho@gmail.com>
2024-09-28 21:13:43 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My ToDo List</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
