b740b8ffc03cc938623634416fac8ca9d34cbd38
lexyxc <elias.w.terho@gmail.com>
2024-09-30 05:57:15 +0300

    Trying to resolve console error #4

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+  },
 });
